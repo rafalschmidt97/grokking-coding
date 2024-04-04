4f73323690707f4a67606d80151c33b8cf238059
Rafał Schmidt <rafalschmidt97@gmail.com>
2024-04-04 12:00:13 +0200

    feat: add multiple test cases

--- designgurus/cip/1-dsaa/1-big-o/2-binary-search/binary_search_test.go
+++ designgurus/cip/1-dsaa/1-big-o/2-binary-search/binary_search_test.go
@@ -1,17 +1,29 @@
 package main
 
 import (
+	"fmt"
 	"testing"
 )
 
 func TestBinarySearch(t *testing.T) {
-	array := []int{2, 3, 4, 5, 10, 40}
-	x := 3
-	want := 1
+	tests := []struct {
+		array []int
+		x     int
+		want  int
+	}{
+		{array: []int{2, 3, 4, 5, 10, 40}, x: 3, want: 1},
+		{array: []int{2, 3, 4, 5, 10, 40}, x: 10, want: 4},
+		{array: []int{2, 3, 4, 5, 10, 40}, x: 2, want: 0},
+	}
 
-	got := binarySearch(array, x)
+	for _, tt := range tests {
+		testname := fmt.Sprintf("%v,%v", tt.array, tt.want)
+		t.Run(testname, func(t *testing.T) {
+			got := binarySearch(tt.array, tt.x)
 
-	if got != want {
-		t.Errorf("got %v; want %v", got, want)
+			if got != tt.want {
+				t.Errorf("got %v; want %v", got, tt.want)
+			}
+		})
 	}
 }
