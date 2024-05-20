b3374be62dd2f209e7c25cc70a11f80683757760
Rafał Schmidt <rafalschmidt97@gmail.com>
2024-05-20 21:03:55 +0200

    feat: remove stars

--- designgurus/cip/1-dsaa/04-stack/8-remove-stars/remove_stars.go
+++ designgurus/cip/1-dsaa/04-stack/8-remove-stars/remove_stars.go
@@ -2,15 +2,27 @@ package main
 
 import "fmt"
 
-// TODO:
-//
-// Time complexity: TODO:
-// Space complexity: TODO:
+// Time complexity: O(n)
+// Space complexity: O(n)
 func removeStars(input string) string {
-	return ""
+	stack := []rune{}
+
+	for _, v := range input {
+		if v == '*' {
+			if len(stack) > 0 {
+				stack = stack[:len(stack)-1]
+			}
+
+			// skip otherwise
+		} else {
+			stack = append(stack, v)
+		}
+	}
+
+	return string(stack)
 }
 
 func main() {
-	input := ""
+	input := "abc*de*f"
 	fmt.Println(removeStars(input))
 }
