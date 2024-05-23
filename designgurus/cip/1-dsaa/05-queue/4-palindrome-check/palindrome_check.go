af50e8fd0a9853363d4b767ecc75a6e68e630280
Rafał Schmidt <rafalschmidt97@gmail.com>
2024-05-24 00:10:29 +0200

    feat: implement palindrome check

--- designgurus/cip/1-dsaa/05-queue/4-palindrome-check/palindrome_check.go
+++ designgurus/cip/1-dsaa/05-queue/4-palindrome-check/palindrome_check.go
@@ -1,10 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
-// Time complexity: TODO:
-// Space complexity: TODO:
+// Time complexity: O(n)
+// Space complexity: O(n)
 func palindromeCheck(input string) bool {
+	normalizedInput := strings.ReplaceAll(strings.ToLower(input), " ", "")
+
+	for len(normalizedInput) > 1 {
+		if normalizedInput[0] != normalizedInput[len(normalizedInput)-1] {
+			return false
+		}
+		normalizedInput = normalizedInput[1 : len(normalizedInput)-1] // dequeue from each side
+	}
+
 	return true
 }
 
