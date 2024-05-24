e8601d8e76819d6e558d1fabcfa7058896e29b5e
Rafał Schmidt <rafalschmidt97@gmail.com>
2024-05-25 00:02:42 +0200

    feat: max subarrays

--- designgurus/cip/1-dsaa/05-queue/6-max-of-subarray/max_subarray.go
+++ designgurus/cip/1-dsaa/05-queue/6-max-of-subarray/max_subarray.go
@@ -2,10 +2,23 @@ package main
 
 import "fmt"
 
-// Time complexity: TODO:
-// Space complexity: TODO:
+// Time complexity: O(n)
+// Space complexity: O(n)
 func maxOfSubarray(input []int, k int) []int {
-	return []int{}
+	queue := []int{}
+
+	iv := [2]int{0, input[0]} // index and value tuple
+	for i, v := range input {
+		if v >= iv[1] || i > iv[0]+k {
+			iv = [2]int{i, v}
+		}
+
+		if i+1 >= k {
+			queue = append(queue, iv[1])
+		}
+	}
+
+	return queue
 }
 
 func main() {
