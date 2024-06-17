4f9f77258865a0106e9585f57bdeaaaf114dec62
Rafał Schmidt <rafalschmidt97@gmail.com>
2024-06-17 23:49:15 +0200

    feat: min diff using traversal

--- designgurus/cip/1-dsaa/07-tree/3-min-diff-bst-nodes/min_diff_bst.go
+++ designgurus/cip/1-dsaa/07-tree/3-min-diff-bst-nodes/min_diff_bst.go
@@ -1,13 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
-// TODO:
+// A binary search tree has an property where if you perform an in-order traversal,
+// the result is a list of numbers in ascending order.
 //
-// Time complexity: TODO:
-// Space complexity: TODO:
+// Time complexity: O(n)
+// Space complexity: O(n)
+
+type Solution struct {
+	Nodes []int
+}
+
 func minimumDifferenceBetweenBst(input *TreeNode) int {
-	return 0
+	solution := Solution{
+		Nodes: make([]int, 0),
+	}
+
+	inorderTraversal(input, &solution)
+
+	// Initialize the minimum difference to the largest possible value.
+	minDiff := math.MaxInt32
+
+	// Loop through the list and find the difference between each consecutive pair.
+	for i := 1; i < len(solution.Nodes); i++ {
+		minDiff = min(minDiff, solution.Nodes[i]-solution.Nodes[i-1])
+	}
+
+	return minDiff
+}
+
+func inorderTraversal(input *TreeNode, solution *Solution) {
+	if input == nil {
+		return
+	}
+	inorderTraversal(input.Left, solution)
+	solution.Nodes = append(solution.Nodes, input.Value)
+	inorderTraversal(input.Right, solution)
 }
 
 func main() {
