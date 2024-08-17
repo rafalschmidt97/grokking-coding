2d352375ac6d6d3b9e39f26e62c536bbdd152d50
Rafał Schmidt <rafalschmidt97@gmail.com>
2024-08-17 11:20:21 +0200

    fix: add intro to vertices to reach

--- src/1-dsaa/11-graph/3-vertices-to-reach-nodes/vertices_to_reach_nodes.go
+++ src/1-dsaa/11-graph/3-vertices-to-reach-nodes/vertices_to_reach_nodes.go
@@ -2,6 +2,15 @@ package main
 
 import "fmt"
 
+// To solve the problem of determining the minimum number of vertices needed to reach
+// all nodes in a directed graph, we focus on the concept of "in-degree" which
+// represents the number of incoming edges to a node. In a directed graph,
+// if a node doesn't have any incoming edges (in-degree of 0), then it means that
+// the node cannot be reached from any other node. Hence, such nodes are mandatory
+// starting points to ensure that every node in the graph can be reached.
+// Our algorithm thus identifies all nodes with an in-degree of 0 as they are
+// potential starting points to traverse the entire graph.
+//
 // Time complexity: O(e+n)
 // Space complexity: O(n)
 func verticesToReach(nodes int, edges [][]int) []int {
