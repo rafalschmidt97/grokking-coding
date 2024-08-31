4a4e502f09873b0ab990ec9ad899f28cca936bb5
Rafał Schmidt <rafalschmidt97@gmail.com>
2024-08-31 19:47:35 +0200

    feat: radix sort

--- src/1-dsaa/13-sorting/7-radix-sort/README.md
+++ src/1-dsaa/13-sorting/7-radix-sort/README.md
@@ -3,4 +3,10 @@
 > A non-comparative sorting algorithm that sorts integers digit by digit
 > starting from the least significant digit to the most.
 
+Radix sort is a non-comparative sorting algorithm that **sorts numbers digit by
+digit starting from the least significant digit** to the most significant digit.
+Radix sort uses counting sort as an intermediate sorting method to sort digits.
+Because it processes individual digits, it can handle large numbers efficiently,
+provided the digits are uniformly distributed.
+
 ## Step-by-Step Algorithm
--- src/1-dsaa/13-sorting/7-radix-sort/radix_sort.go
+++ src/1-dsaa/13-sorting/7-radix-sort/radix_sort.go
@@ -2,7 +2,51 @@ package main
 
 import "fmt"
 
-func radixSort(array []int) []int {
+func radixSort(arr []int) []int {
+	maximum := 0
+	for _, num := range arr {
+		if num > maximum {
+			maximum = num
+		}
+	}
+
+	// Do counting sort for every digit.
+	// The exp is 10^i where i is the current digit number
+	for exp := 1; maximum/exp > 0; exp *= 10 {
+		countSort(arr, exp)
+	}
+
+	return arr
+}
+
+// Function to perform counting sort on the array according to the
+// digit represented by exp (10^i)
+func countSort(arr []int, exp int) {
+	output := make([]int, len(arr)) // output array
+	count := make([]int, 10)
+	var i int
+
+	// Store count of occurrences in count[]
+	for i = 0; i < len(arr); i++ {
+		count[(arr[i]/exp)%10]++
+	}
+
+	// Change count[i] so that it now contains actual position of this digit in output[]
+	for i = 1; i < 10; i++ {
+		count[i] += count[i-1]
+	}
+
+	// Build the output array
+	for i = len(arr) - 1; i >= 0; i-- {
+		output[count[(arr[i]/exp)%10]-1] = arr[i]
+		count[(arr[i]/exp)%10]--
+	}
+
+	// Copy the output array to arr[], so that arr[] now contains sorted
+	// numbers according to current digit
+	for i = 0; i < len(arr); i++ {
+		arr[i] = output[i]
+	}
 }
 
 func main() {
